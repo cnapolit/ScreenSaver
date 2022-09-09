90fd77030357d0c338620eab6769f9c3dfd6298b
Cameron Napolitano <cameronnapolitano@yahoo.com>
2022-09-09 17:14:40 -0400

    [#28] Option to round corners of video

--- Models/ScreenSaverSettings.cs
+++ Models/ScreenSaverSettings.cs
@@ -1,7 +1,6 @@
 ﻿using ScreenSaver.Models.Enums;
 using System.Collections.Generic;
 
-
 namespace ScreenSaver.Models
 {
     public class ScreenSaverSettings : ObservableObject
@@ -9,8 +8,12 @@ namespace ScreenSaver.Models
         public uint GameTransitionInterval { get; set; } = 20;
         public uint ScreenSaverInterval { get; set; } = 90;
         public uint Volume { get; set; } = 50;
-        public AudioSource AudioSource { get; set; } = AudioSource.Music;
-        public PlayState PlayState { get; set; } = PlayState.FullScreen;
+        public uint VideoCornerRadius { get; set; } = 20;
+        public uint ClockCornerRadius { get; set; } = 20;
+        public uint ClockFontSize { get; set; } = 40;
+        public uint ClockSubFontSize { get; set; } = 30;
+        public string ClockFont { get; set; } = "Segoe UI Light";
+        public bool DisplayClock { get; set; } = true;
         public bool PlayBackup { get; set; } = false;
         public bool BackgroundSkip { get; set; } = true;
         public bool VideoSkip { get; set; } = false;
@@ -20,6 +23,7 @@ namespace ScreenSaver.Models
         public bool IncludeLogo { get; set; } = true;
         public bool DisableWhilePlaying { get; set; } = true;
         public bool PauseOnDeactivate { get; set; } = true;
-        public bool DisplayClock { get; set; } = true;
+        public AudioSource AudioSource { get; set; } = AudioSource.Music;
+        public PlayState PlayState { get; set; } = PlayState.FullScreen;
     }
 }
